1643599573d1f2462fc5a0c0e8131d418caba3d6
Kuba <jakub.jaszcz538@gmail.com>
2025-03-28 19:15:31 +0100

    feat: tailwind installed

--- kuracjusz_go_frontend/src/App.tsx
+++ kuracjusz_go_frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
--- kuracjusz_go_frontend/src/pages/Home.tsx
+++ kuracjusz_go_frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1>Kuracjusz GO HOMEPAGE</h1>
+      <h1 className="text-4xl font-bold">Kuracjusz GO HOMEPAGE</h1>
     </div>
   );
 };
